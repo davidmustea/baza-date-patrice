32413a9a795d2dc915fd441e1c1cb4b12c53f674
davidmustea <davidmustea18@gmail.com>
2024-02-20 18:43:36 +0200

    form added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return redirect(url_for("search"))
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        d_search = request.form["search-bar"]
+        return redirect(url_for("user", usr = d_search))
+    if request.method == "GET":
+        return render_template("search.html")
+
+#@app.route("/<usr>")
+#def user(usr):
+#    return f"salut {usr}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-@app.route('/')
-def home():
-    return redirect(url_for('search'))
-
-@app.route('/search')
-def search():
-    return render_template("search.html")
